75b8f602d4fb9ec75f2dcd68380a6a9fe6f2fd26
马文卿 <529837505@qq.com>
2022-11-14 14:07:18 +0800

    feat: update article tag

--- controller/blog.js
+++ controller/blog.js
@@ -58,7 +58,8 @@ const newBlog = async (blogData = {}) => {
 const updateBlog = async (id, blogData = {}) => {
     const title = xss(blogData.title);
     const content = xss(blogData.content)
-    const sql = `update blogs set title='${title}', content="${content}" where id=${id};`
+    const tag = blogData.tag
+    const sql = `update blogs set title='${title}', content="${content}", tag='${tag}' where id=${id};`
     const updateData = await exec(sql);
     if(updateData.affectedRows > 0) {
         return true
