4c09e265e3e489e1d6efe58a3fd7f7430e248a56
马文卿 <529837505@qq.com>
2022-11-25 15:06:53 +0800

    feat: comment

--- controller/comment.js
+++ controller/comment.js
@@ -23,10 +23,11 @@ const getCommentList = async (query) => {
     let totality = {};
     const current = query.current || 1;
     const pageSize = query.pageSize || 10;
-    const sql = `select comment.*, b.name as toName, b.content as toContent from comment left join (select * from comment where toId is null) b on comment.toId = b.id order by createTime DESC limit ${(current-1) * pageSize},${pageSize};`
+    const sql = `select comment.*, b.name as toName, b.content as toContent from comment left join (select * from comment where 1=1) b on comment.toId = b.id order by createTime DESC limit ${(current-1) * pageSize},${pageSize};`
     const sqlCnt = `SELECT COUNT(id) total FROM comment;`
     const list = await exec(sql);
     const res = await exec(sqlCnt);
+    console.log(list);
     totality.list = list;
     totality.total = res[0].total;
     totality.current = Number(current);
